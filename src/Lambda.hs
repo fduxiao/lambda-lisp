96e932b1de7bc5b1216bec17d95fac05a7324d10
fduxiao <fduxiao@hotmail.com>
2020-08-17 01:00:23 -0400

    lambda calculus

---
+++ src/Lambda.hs
@@ -0,0 +1,73 @@
+module Lambda (
+    Lambda, Parser, parseLambda, reduce, normalForm, rename, parseReduce
+) where
+
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import Text.Parsec.Language (emptyDef)
+import qualified Text.Parsec.Token as Tok
+
+type V = String
+data Lambda = Var V | Lambda V Lambda | App Lambda Lambda
+
+-- beta reduction
+rename :: String -> Lambda -> Lambda -> Lambda
+rename from to t@(Var v) = if v == from then to else t
+rename from to t@(Lambda v term) = if v == from then t else Lambda v (rename from to term)
+rename from to (App a b) = App (rename from to a) (rename from to b)
+
+normalForm :: Lambda -> Bool
+normalForm (Var v) = True
+normalForm (Lambda _ term) = normalForm term
+normalForm (App (Lambda _ _) b) = False
+normalForm (App a b) = normalForm a && normalForm b
+
+reduce :: Lambda -> Lambda
+reduce (Var v) = Var v
+reduce (Lambda x term) = Lambda x (reduce term)
+reduce (App (Lambda x term) b) = rename x b term
+reduce (App a b) = App (reduce a) (reduce b)
+
+-- from AST to list
+instance Show Lambda where
+    show (Var v) = v
+    show (Lambda v term) = "(lambda " ++ v ++ " " ++ show term ++ ")"
+    show (App a b) = "(" ++ show a ++ " " ++ show b ++ ")"
+
+-- the following is a parser
+-- lexer
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser emptyDef {Tok.reservedNames = ["lambda"]}
+
+parens :: Parser a -> Parser a
+parens a = try (Tok.parens lexer a) <|> Tok.braces lexer a
+
+reserved :: String -> Parser ()
+reserved = Tok.reserved lexer
+
+-- parser
+
+var :: Parser String
+var = try (Tok.identifier lexer) <|> Tok.operator lexer
+
+abstraction :: Parser Lambda
+abstraction = parens $ do
+    reserved "lambda"
+    x <- var
+    Lambda x <$> lambda
+
+app :: Parser Lambda
+app = parens $ do
+    a <- lambda
+    App a <$> lambda
+
+lambda :: Parser Lambda
+lambda = try (Var <$> var) <|> try abstraction <|> app
+
+parseLambda :: String -> Either ParseError Lambda
+parseLambda = parse lambda "stdin"
+
+parseReduce :: String -> Either ParseError Lambda
+parseReduce s = do
+    l <- parseLambda s
+    return $ reduce l
